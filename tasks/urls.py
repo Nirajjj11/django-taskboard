43c365243e2f9b76f84a92fe3842d1e8e7b7a369
Your Name <nirajkumar09160@gmail.com>
2026-01-09 21:04:57 +0530

    Task of Day 2 done

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="task"),
+    path("add-task/", views.add_task, name="add_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Task
+from django.http import JsonResponse
 
 def home(request):
     data = {
@@ -9,3 +10,14 @@ def home(request):
 
 
     return render(request, "task.html", data)
+
+
+def add_task(request):
+    if request.method == "POST":
+        task = Task.objects.create(
+            title=request.POST.get("title"),
+            description=request.POST.get("description")
+        )
+        return JsonResponse({"status": "saved", "id": task.id})
+
+    return render(request, "entertask.html", {"title": "Task Entry"})
